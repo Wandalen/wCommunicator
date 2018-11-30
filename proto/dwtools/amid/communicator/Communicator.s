6b2f86e5b97452a4788b0420eb90efc8053ba850
vmigdal <volodymyr.migdal@gmail.com>
2018-11-30 17:22:17 +0200

    repair require

--- proto/dwtools/amid/communicator/Communicator.s
+++ proto/dwtools/amid/communicator/Communicator.s
@@ -13,7 +13,7 @@
 if( typeof module !== 'undefined' )
 {
 
-  let _ = require( '../../../Tools.s' );
+  let _ = require( '../../Tools.s' );
 
   _.include( 'wCopyable' );
   _.include( 'wEventHandler' );
